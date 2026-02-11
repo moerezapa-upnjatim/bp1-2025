f8a080cfbbd51602e798b6777e03ac777737bace
Muhammad Reza Pahlawan <muhammad_reza.si@upnjatim.ac.id>
2026-02-11 13:14:17 +0700

    backup

---
+++ kelasB/week5.java
@@ -0,0 +1,15 @@
+// package Class B;
+
+public class week5 {
+    
+    public static void main(String[] args) {
+        
+        // potd - 1
+        int saldoPakHato = 57000;
+        int uangDiambil = 100000;
+        
+        if (uangDiambil <= saldoPakHato) {
+            System.out.println("Mohon ditunggu. Silahkan ambil uang Anda");
+        }
+    }
+}
